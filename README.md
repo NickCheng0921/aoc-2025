99b49a196bf5cb330437544fc4c359820d93843b
Your Name <you@example.com>
2025-12-01 22:14:38 -0800

    Solved star 2 on day 1

--- README.md
+++ README.md
@@ -2,4 +2,11 @@ Advent of Code for 2025
 
 | Day | Star 1 | Star 2 |
 |---|---|---|
-| 1 | ✓ | ✖ |
+| 1 | ✓ | ✓ |
+
+
+
+
+Placeholders
+
+✓ | ✖
--- solutions/day1_p2.py
+++ solutions/day1_p2.py
@@ -9,6 +9,7 @@ pos 11, R8, pos 19
 Actual password is # of times dial clicks past 0 while following list
 
 5660, 6156, 5748 too high
+5657 right
 """
 
 dial, click0 = 50, 0
@@ -19,22 +20,35 @@ with open("../input/day1.txt", "r") as f:
 #  verify parsing
 #print(rotation_list[:5], rotation_list[-5:])
 
+# click it
+
+# click0 = 0
+# for rotation in rotation_list:
+#     direction, amount = rotation[:1], int(rotation[1:])
+
+#     if direction == "R":
+#         for click in range(amount):
+#             dial += 1
+#             click0 += (dial % 100) == 0
+#         dial = dial%100
+#     else:
+#         for click in range(amount):
+#             dial -= 1
+#             click0 += (abs(dial)%100) == 0
+#         dial = dial%100
+
+# print(click0)
+
+
 for rotation in rotation_list:
     direction, amount = rotation[:1], int(rotation[1:])
     sign = 1 if direction == "R" else -1
 
-    intermed_dial = dial + sign * amount
-
     if sign > 0:
-        # number of times we wrapped going right
-        click0 += intermed_dial // 100
+        click0 += (dial + sign*amount) // 100
     else:
-        # number of times we wrapped going left
-        if intermed_dial <= 0:
-            click0 += 1 + (-intermed_dial) // 100
+        click0 += -(dial == 0) + ((100 - dial) + amount) // 100
+    
+    dial = (dial + sign*amount) % 100
 
-    if intermed_dial < 0:
-        intermed_dial = 100 - ((-intermed_dial)%100)
-    dial = intermed_dial % 100
-
-print(click0, dial)
+print(click0)
